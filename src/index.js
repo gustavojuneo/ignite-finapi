03285db638d04ae92e02d9ec0bea68e3992f4fdc
Gustavo Juneo Santos <gustavojuneo@outlook.com>
2022-04-03 23:30:05 -0300

    create account and statement routes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,48 @@
 const express = require('express');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(express.json());
+
+const costumers = [];
+
+/**
+ * cpf - string
+ * name - string
+ * id - uuid
+ * statement []
+ */
+app.post('/account', (request, response) => {
+  const { cpf, name } = request.body;
+
+  const customerAlreadyExists = costumers.some(
+    customer => customer.cpf === cpf,
+  );
+
+  if (customerAlreadyExists) {
+    return response.status(400).json({ error: 'Customer already exists!' });
+  }
+
+  const id = uuidv4();
+  const account = {
+    id,
+    cpf,
+    name,
+    statement: [],
+  };
+
+  costumers.push(account);
+
+  return response.status(201).send();
+});
+
+app.get('/statement/:cpf', (request, response) => {
+  const { cpf } = request.params;
+
+  const customer = costumers.find(customer => customer.cpf === cpf);
+
+  return response.json(customer.statement);
+});
 
 app.listen(3333);
